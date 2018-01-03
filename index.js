7da532be74d18009bb942df4823b8406de783f49
LiChangjun <changjun.li@ele.me>
2018-01-03 18:06:04 +0800

    implement sync and async

--- index.js
+++ index.js
@@ -2,27 +2,58 @@ const fs = require('fs')
 const path = require('path')
 const chalk = require('chalk')
 
-function copyFile(baseDirectory, targetDirectory) {
-  fs.readdirSync(baseDirectory).forEach(fileOrDirector => {
-    const filePath = path.resolve(baseDirectory, fileOrDirector)
+function copyDirSync(sourceDirectory, targetDirectory) {
+  fs.readdirSync(sourceDirectory).forEach(fileOrDirector => {
+    const filePath = path.resolve(sourceDirectory, fileOrDirector)
     
-    fs.stat(filePath, (error, stats) => {
-      if (error) {
-        console.error(error)
-        return
-      }
-      if (stats.isFile()) {
-        console.info('copy ' + chalk.green(fileOrDirector) + ' to build-zip directory')
-        fs.copyFileSync(filePath, path.resolve(targetDirectory, fileOrDirector))
-      }
-      if (stats.isDirectory()) {
-        const newBaseDir = path.resolve(baseDirectory, fileOrDirector)
-        const newTargetDir = path.resolve(targetDirectory, fileOrDirector)
-        fs.mkdirSync(newTargetDir)
-        copyFile(newBaseDir, newTargetDir)
-      }
+    const fsStats = fs.statSync(filePath)
+    if (fsStats.isFile()) {
+      console.info(
+        'copy ' +
+        chalk.green(fileOrDirector) +
+        ` to ${ targetDirectory.substr(targetDirectory.lastIndexOf('/') + 1) } directory`)
+      fs.copyFileSync(filePath, path.resolve(targetDirectory, fileOrDirector))
+    }
+    if (fsStats.isDirectory()) {
+      const newSourceDir = path.resolve(sourceDirectory, fileOrDirector)
+      const newTargetDir = path.resolve(targetDirectory, fileOrDirector)
+      fs.mkdir(newTargetDir)
+      copyDirSync(newSourceDir, newTargetDir)
+    }
+  })
+}
+
+function copyDirAsync(sourceDirectory, targetDirectory ) {
+  fs.readdir(sourceDirectory, (error, filesOrDirectory) => {
+    // 遍历文件或者目录的数组
+    files.forEach(file => {
+      const filePath = path.resolve(sourceDirectory, filesOrDirectory)
+
+      fs.stat(filePath, (error, stats) => {
+        if (stats.isFile()) {
+          console.info(
+            'copy ' +
+            chalk.green(fileOrDirector) +
+            ` to ${ targetDirectory.substr(targetDirectory.lastIndexOf('/') + 1) } directory`)
+          fs.copyFile(filePath, path.resolve(targetDirectory, fileOrDirector))
+        }
+        
+        if (stats.isDirectory()) {
+          const newSourceDir = path.resolve(sourceDirectory, fileOrDirector)
+          const newTargetDir = path.resolve(targetDirectory, fileOrDirector)
+          
+          fs.mkdirSync(newTargetDir, (error) => {
+            copyDirAsync(newSourceDir, newTargetDir)
+          })
+        }
+      })
     })
   })
 }
 
-module.exports = copyFile
+function copyDir(sourceDirectory, targetDirectory) {
+  copyDirAsync(sourceDirectory, targetDirectory)
+}
+copyDir.copyDirSync = copyDirSync
+
+module.exports = copyDir
